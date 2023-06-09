17ec3f16da2a0927a649a38a7f06dec57c1cb6f9
amdonatusprince <donatusprince@gmail.com>
2023-06-09 17:34:05 +0100

    updated Coming Soon Page

--- src/components/Countdown/Timer.jsx
+++ src/components/Countdown/Timer.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 // import moment from 'moment';
-import "./Timer.css";
+import "./Time r.css";
 
 class CountDown extends Component {
   constructor(props) {
--- src/pages/Soon/Soon.jsx
+++ src/pages/Soon/Soon.jsx
@@ -30,30 +30,7 @@ const Soon = () => {
         </div>
       </div>
     </div>
-
-
-        {/* <div className={design.Grow_left}>
-          <img src={STAR} alt='' />
-          <p>Trusted by 3456+ users worldwide</p>
-          <div className={design.Grow_left_middle}>
-            <h1>
-              Share Your Story with <span>Spacestar</span>{' '}
-            </h1>
-            <p>
-            A safe haven where you can freely share your thoughts, emotions, and experiences, knowing that you are not alone.
-            </p>
-          </div>
-          <div className={design.Navbar}>
-          <Link to='/comingsoon'>
-            <Button  content='Get started'/>
-          </Link>
-          </div>
-
-        </div> */}
-        {/* <div className={design.Grow_right}>
-          <img src={RIGHT} alt='' />
-        </div> */}
-        {/* {' '} */}
+    
       </div>
     </div>
   );
