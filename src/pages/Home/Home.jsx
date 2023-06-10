edb1f8104d7d29cce5a42d4ee9397807b752ce05
onyekachi <emegwaonyeka@gmail.com>
2023-06-10 14:55:23 +0100

    final changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,14 +2,19 @@ import Grow from '../../components/Grow/Grow';
 import Navbar from '../../components/Navbar/Navbar';
 import Why from '../../components/Why/Why';
 import GetStarted from '../../components/GetStarted/GetStarted';
+import Footer from '../../components/Footer/Footer';
 
 const Home = () => {
   return (
-    <div>
+    <div style={{
+      maxWidth: '1400px',
+      margin: '0 auto'
+    }}>
       <Navbar />
       <Grow />
       <Why />
       <GetStarted/>
+      <Footer/>
     </div>
   );
 };
